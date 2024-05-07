ca568a0245d1ec3b38a6d097e319b2e5780ae53a
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-05-06 22:12:53 -0400

    Creating basic medical form keeping track of age.

---
+++ lib/form.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Form"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: const Text("Name: _____"),
+          ),
+          Container(
+            child: const FormInputs(),
+          ),
+          Expanded(
+            child: Image.asset('assets/red-cross-background.jpg',
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+            )
+          )
+        ],
+        )
+    );
+  }
+}
+
+class FormInputs extends StatefulWidget {
+  const FormInputs({super.key});
+
+  @override
+  State<FormInputs> createState() => _FormInputsState();
+}
+
+class _FormInputsState extends State<FormInputs> {
+  int age = 0;
+
+  void increaseAge() {
+    setState(() {
+      age += 1;
+    });
+  }
+  void decreaseAge() {
+    setState(() {
+      if(age > 0) {
+        age -= 1;
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('Age: ${age}'),
+            const Expanded(child: const SizedBox()),
+            TextButton(
+              onPressed: decreaseAge, 
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue[600],
+                foregroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5))
+                )
+              ),
+              child: const Text('-')),
+            TextButton(
+              onPressed: increaseAge, 
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red[600],
+                foregroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5))
+                )
+              ),
+              child: const Text('+')),
+          ],
+          )
+
+      ],
+    );
+  }
+}
