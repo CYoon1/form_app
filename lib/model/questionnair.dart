062d7dedf99e983974f0c1ae33bbd2838fbfe930
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-05-21 19:38:10 -0400

    Created basic data model

---
+++ lib/model/questionnair.dart
@@ -0,0 +1,18 @@
+class Questionnair {
+
+  Questionnair({
+    required this.name,
+  });
+
+  final String name;
+  int painIntensity = 0;
+  int careDifficulty = 0;
+  int liftDifficulty = 0;
+
+  int walkDifficulty = 0;
+  int sitDifficulty = 0;
+
+  int standDifficulty = 0;
+  int sleepDifficulty = 0;
+  
+}
